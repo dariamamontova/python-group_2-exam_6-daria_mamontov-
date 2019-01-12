8bddcc76a5aa36053a388212e03363d15b830361
Daria <mamontova.dasha@gmail.com>
2019-01-12 14:41:06 +0600

    Созданы модели пользователя и публикации, модели зарегестрированы в админке. Добалена папка uploads.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from webapp.models import UserInfo, Post
+
+class UserinfoInline(admin.StackedInline):
+    model = UserInfo
+
+
+class UserinfoAdmin(UserAdmin):
+    inlines = [UserinfoInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserinfoAdmin)
+admin.site.register(Post)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='user_info', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+    picture = models.ImageField(verbose_name='Фотография')
+    friend = models.ManyToManyField('UserInfo', blank=True, related_name='user_friend', verbose_name='Друг')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    date = models.DateField(verbose_name='Дата')
+    author = models.ForeignKey(UserInfo, on_delete=models.PROTECT, related_name='post_author', verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
